6bbcc87221c18fd3e6217f70823f4dc0be2d4464
SaraRouichi <sararouichi94@gmail.com>
2023-09-07 17:51:17 -0700

    C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main -this is the main function
- *
- * Return: 0 when successfull
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+return (0);
 }
 
