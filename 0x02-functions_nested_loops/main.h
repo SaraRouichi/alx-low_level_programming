e561c77e972b462b0b8d19cf61581c94ae8b9960
SaraRouichi <sararouichi94@gmail.com>
2023-09-13 05:45:51 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef main_h
-#define main_h
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -15,5 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-endif
 
